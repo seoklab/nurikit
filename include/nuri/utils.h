b96737d12ca154230bd88eb410e98002d5d11704
jnooree <jnooree@snu.ac.kr>
2023-05-08 15:13:16 +0900

    feat(core): use `std::list` for edge list

--- include/nuri/utils.h
+++ include/nuri/utils.h
@@ -6,8 +6,10 @@
 #ifndef NURI_UTILS_H_
 #define NURI_UTILS_H_
 
+#include <algorithm>
 #include <numeric>
 #include <type_traits>
+#include <vector>
 
 namespace nuri {
 namespace internal {
@@ -19,6 +21,39 @@ namespace internal {
   template <bool is_const, class T>
   using const_if_t = typename const_if<is_const, T>::type;
 }  // namespace internal
+
+#if __cplusplus >= 202002L
+using std::erase;
+using std::erase_if;
+#else
+template <class T, class Alloc, class U>
+typename std::vector<T, Alloc>::size_type erase(std::vector<T, Alloc> &c,
+                                                const U &value) {
+  auto it = std::remove(c.begin(), c.end(), value);
+  auto r = std::distance(it, c.end());
+  c.erase(it, c.end());
+  return r;
+}
+
+template <class T, class Alloc, class Pred>
+typename std::vector<T, Alloc>::size_type erase_if(std::vector<T, Alloc> &c,
+                                                   Pred pred) {
+  auto it = std::remove_if(c.begin(), c.end(), pred);
+  auto r = std::distance(it, c.end());
+  c.erase(it, c.end());
+  return r;
+}
+#endif
+
+template <class T, class Alloc, class Pred>
+typename std::vector<T, Alloc>::iterator erase_first(std::vector<T, Alloc> &c,
+                                                     Pred pred) {
+  auto it = std::find_if(c.begin(), c.end(), pred);
+  if (it != c.end()) {
+    c.erase(it);
+  }
+  return it;
+}
 
 template <class Container>
 Container mask_to_map(const Container &mask) {
