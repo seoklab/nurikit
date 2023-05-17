520cabd62b112fd523b4752925821295b58378c4
jnooree <jnooree@snu.ac.kr>
2023-05-17 17:11:19 +0900

    build(test/core): update test build settings

--- cmake/NurikitTest.cmake
+++ cmake/NurikitTest.cmake
@@ -9,12 +9,22 @@ endif()
 
 include(GoogleTest)
 
-function(nurikit_add_test name)
+function(nurikit_add_single_test name)
   set(target_name nurikit_${name})
   add_executable("${target_name}" ${name}.cpp)
   target_link_libraries("${target_name}"
     PRIVATE GTest::gtest GTest::gtest_main absl::absl_log absl::absl_check)
 
+  if(TARGET nuri_lib)
+    target_link_libraries("${target_name}" PRIVATE nuri_lib)
+  endif()
+
   gtest_discover_tests("${target_name}" "" AUTO)
   add_dependencies(nurikit_all_test "${target_name}")
+endfunction()
+
+function(nurikit_add_test)
+  foreach(name ${ARGN})
+    nurikit_add_single_test(${name})
+  endforeach()
 endfunction()
