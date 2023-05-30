3772e9ffb8099bd03631254e3f5b925838193123
jnooree <jnooree@snu.ac.kr>
2023-05-30 14:22:35 +0900

    feat(python): log absl messages via python logging
    ...if nurikit is loaded from a python interpreter.

--- python/nuri/__init__.py
+++ python/nuri/__init__.py
@@ -17,3 +17,12 @@ except ImportError:
     __version__ = __full_version__ = "unknown"
 
 from .core import periodic_table
+
+try:
+    from ._log_adapter import _init as _init_logging
+    _init_logging()
+    del _init_logging
+except Exception:
+    import logging as _logging
+    _logging.warning(
+        "failed to initialize logging for nurikit.", exc_info=True)
