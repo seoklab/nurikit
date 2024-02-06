f699ce9595ca5d29c002a5f3ccd796291089bd55
Nuri Jung <jnooree@snu.ac.kr>
2024-02-06 15:43:58 +0900

    feat: add cyclic indexer for eigen types

--- include/nuri/eigen_config.h
+++ include/nuri/eigen_config.h
@@ -9,6 +9,8 @@
 
 #include <Eigen/Dense>
 
+#include <absl/log/absl_check.h>
+
 namespace nuri {
 template <class DT, int Rows, int Cols, int... Extra>
 using Matrix = Eigen::Matrix<DT, Rows, Cols, Eigen::RowMajor, Extra...>;
@@ -95,6 +97,71 @@ auto swap_axis(Eigen::PlainObjectBase<MatrixLike> &m) {
   return Eigen::Map<ReturnType, Eigen::AlignedMax>(m.data(), m.cols(),
                                                    m.rows());
 }
+
+/**
+ * @brief Cyclic indexer for Eigen types with a given offset.
+ * @tparam O Offset of the cyclic index. If 0, it is determined at runtime.
+ * @tparam N Size of the cyclic index. If 0, it is determined at runtime.
+ *
+ * This will produce an index that is cyclic with a given offset, i.e.,
+ * (O, O+1, ..., N-1, 0, 1, ..., O-1).
+ */
+template <int O = 0, int N = 0>
+class CyclicIndex {
+public:
+  static_assert(N > 0, "N must be positive");
+
+  constexpr static int size() { return N; }
+
+  constexpr int operator[](int i) const {
+    int before = O + i, after = i - kZeroAt;
+    return i < kZeroAt ? before : after;
+  }
+
+private:
+  constexpr static int kZeroAt = (N - O) % N;
+  static_assert(kZeroAt >= 0, "Offset must be smaller than size");
+};
+
+template <int O>
+class CyclicIndex<O, 0> {
+public:
+  constexpr CyclicIndex(int n): n_(n), zero_at_((n - O) % n) {
+    ABSL_DCHECK(zero_at_ >= 0);
+  }
+
+  constexpr int size() const { return n_; }
+
+  constexpr int operator[](int i) const {
+    int before = O + i, after = i - zero_at_;
+    return i < zero_at_ ? before : after;
+  }
+
+private:
+  int n_;
+  int zero_at_;
+};
+
+template <>
+class CyclicIndex<0, 0> {
+public:
+  constexpr CyclicIndex(int n, int offset)
+      : n_(n), offset_(offset), zero_at_((n - offset) % n) {
+    ABSL_DCHECK(zero_at_ >= 0);
+  }
+
+  constexpr int size() const { return n_; }
+
+  constexpr int operator[](int i) const {
+    int before = offset_ + i, after = i - zero_at_;
+    return i < zero_at_ ? before : after;
+  }
+
+private:
+  int n_;
+  int offset_;
+  int zero_at_;
+};
 }  // namespace nuri
 
 #endif /* NURI_EIGEN_CONFIG_H_ */
