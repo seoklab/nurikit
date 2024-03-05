22c5423cb411addda6fcb2c69e2e052bc985c917
Nuri Jung <jnooree@snu.ac.kr>
2024-03-05 14:33:23 +0900

    chore(dev): add pytest and don't start http server in coverage script

--- scripts/check_coverage.sh
+++ scripts/check_coverage.sh
@@ -10,11 +10,16 @@ build_dir="$(realpath "${1-build}")"
 cd "$build_dir"
 ctest -C Debug -T test -"j$(nproc)" --output-on-failure
 
+if [[ "${3-}" == "--python" ]]; then
+	cd "$prj_root/python"
+	pytest -v test
+	cmake --build "$build_dir" --target nuri_python_docs_doctest -"j$(nproc)"
+fi
+
 cd "$prj_root"
 if [[ "${2-}" == "--html" ]]; then
 	mkdir -p "$build_dir/coverage/html"
 	gcovr --html-details "$build_dir/coverage/html/index.html" "$build_dir"
-	python3 -m http.server -d "$build_dir/coverage/html" "${3-8000}"
 elif [[ "${2-}" == "--json" ]]; then
 	mkdir -p "$build_dir/coverage/json"
 	gcovr --coveralls "$build_dir/coverage/json/coverage.json" "$build_dir"
