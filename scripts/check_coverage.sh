cb1cc5e66eec0ec3db360824a8e60c9b2e2c0d17
Nuri Jung <jnooree@snu.ac.kr>
2024-03-06 18:00:28 +0900

    chore(dev): update coverage script

--- scripts/check_coverage.sh
+++ scripts/check_coverage.sh
@@ -5,24 +5,60 @@
 #
 
 prj_root="$(dirname "$(dirname "$(realpath "$0")")")"
+
+cpp_test=false
+py_test=false
+output_args=()
+output=''
+
+while getopts ':-:' opt; do
+	case "$opt" in
+	-) ;;
+	*) exit 1 ;;
+	esac
+
+	case "$OPTARG" in
+	cpp) cpp_test=true ;;
+	python) py_test=true ;;
+	html)
+		output_args=(--html-details)
+		output="coverage/html/index.html"
+		;;
+	json)
+		output_args=(--coveralls)
+		output="coverage/json/coverage.json"
+		;;
+	*)
+		echo >&2 "Unknown option: --$OPTARG"
+		exit 1
+		;;
+	esac
+done
+
+shift $((OPTIND - 1))
 build_dir="$(realpath "${1-build}")"
 
-cd "$build_dir"
-ctest -C Debug -T test -"j$(nproc)" --output-on-failure
-
-if [[ "${3-}" == "--python" ]]; then
-	cd "$prj_root/python"
-	pytest -v test
-	cmake --build "$build_dir" --target nuri_python_docs_doctest -"j$(nproc)"
+if [[ $cpp_test = false && $py_test = false ]]; then
+	cpp_test=true
+	py_test=true
 fi
 
 cd "$prj_root"
-if [[ "${2-}" == "--html" ]]; then
-	mkdir -p "$build_dir/coverage/html"
-	gcovr --html-details "$build_dir/coverage/html/index.html" "$build_dir"
-elif [[ "${2-}" == "--json" ]]; then
-	mkdir -p "$build_dir/coverage/json"
-	gcovr --coveralls "$build_dir/coverage/json/coverage.json" "$build_dir"
-else
-	gcovr "$build_dir"
+
+if [[ $cpp_test = true ]]; then
+	pushd "$build_dir"
+	ctest -C Debug -T test -"j$(nproc)" --output-on-failure
+	popd
 fi
+
+if [[ $py_test = true ]]; then
+	pytest -v -"n$(nproc)" python/test
+	cmake --build "$build_dir" --target nuri_python_docs_doctest -"j$(nproc)"
+fi
+
+if [[ -n $output ]]; then
+	output="$build_dir/$output"
+	mkdir -p "$(dirname "$output")"
+	output_args+=("$output")
+fi
+gcovr "${output_args[@]}" "$build_dir"
