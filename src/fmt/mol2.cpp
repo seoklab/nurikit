2483d663705fc8f7930567bd74123287e8403c45
Nuri Jung <jnooree@snu.ac.kr>
2024-03-18 15:15:45 +0900

    refactor(fmt): move common rules to internal header

--- src/fmt/mol2.cpp
+++ src/fmt/mol2.cpp
@@ -30,6 +30,7 @@
 #include <absl/strings/str_split.h>
 
 #include "nuri/eigen_config.h"
+#include "fmt_internal.h"
 #include "nuri/algo/guess.h"
 #include "nuri/core/element.h"
 #include "nuri/core/molecule.h"
@@ -174,33 +175,6 @@ void atom_data_from_subtype(AtomData &data, int i, std::string_view subtype,
 
 // NOLINTBEGIN(readability-identifier-naming)
 namespace parser {
-template <class T>
-struct TrailingBlanksRuleTag;
-
-template <class T, class Tag = TrailingBlanksRuleTag<T>>
-struct TrailingBlanksRule: public x3::rule<Tag, T> {
-  using Base = x3::rule<Tag, T>;
-  using Base::Base;
-
-  constexpr TrailingBlanksRule(): Base("") { }
-
-  template <class RHS>
-  // NOLINTNEXTLINE(misc-unconventional-assign-operator,cppcoreguidelines-c-copy-assignment-signature)
-  constexpr auto operator=(const RHS &rhs) const && {
-    return Base::operator=(rhs >> +x3::omit[x3::blank]);
-  }
-};
-
-constexpr auto nonblank_trailing_blanks =
-    TrailingBlanksRule<std::string, struct nonblank_trailing_blanks_tag>() =
-        +~x3::blank;
-
-constexpr auto double_trailing_blanks = TrailingBlanksRule<double>() =
-    x3::double_;
-
-constexpr auto uint_trailing_blanks = TrailingBlanksRule<unsigned int>() =
-    x3::uint_;
-
 constexpr auto atom_line = *x3::omit[x3::blank]         //
                            >> uint_trailing_blanks      //
                            >> nonblank_trailing_blanks  //
