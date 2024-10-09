a155b157fc5f58ecdf96a734c33231e214b1b3c8
Nuri Jung <jnooree@snu.ac.kr>
2024-10-09 15:31:35 +0900

    refactor(algo/optim/lbfgsb): reorganize interfaces

--- include/nuri/algo/optim.h
+++ include/nuri/algo/optim.h
@@ -6,6 +6,8 @@
 #ifndef NURI_ALGO_OPTIM_H_
 #define NURI_ALGO_OPTIM_H_
 
+#include <cmath>
+#include <cstdint>
 #include <functional>
 
 #include <Eigen/Dense>
@@ -15,7 +17,10 @@
 
 namespace nuri {
 namespace internal {
-  constexpr inline double kEpsMach = 2.220446049250313e-16;
+  constexpr double kEpsMach = 2.220446049250313e-16;
+
+  extern bool lbfgsb_errclb(const ArrayXd &x, const ArrayXi &nbd,
+                            const Array2Xd &bounds, int m, double factr);
 
   /**
    * nbd == 0x1 if has lower bound,
@@ -64,6 +69,68 @@ namespace internal {
 
     const ArrayXi *nbd_;
     const Array2Xd *bds_;
+  };
+
+  class LbfgsbLnsrch {
+  public:
+    LbfgsbLnsrch(MutRef<ArrayXd> &x, const ArrayXd &t, const ArrayXd &z,
+                 const ArrayXd &d, const LbgfsbBounds &bounds, double f0,
+                 double g0, int iter, bool constrained, bool boxed,
+                 double ftol = 1e-3, double gtol = 0.9,
+                 double xtol = 0.1) noexcept;
+
+    bool search(double f, double g);
+
+    double dtd() const { return dtd_; }
+
+    double step() const { return step_; }
+
+    double xstep() const { return step_ * dnorm_; }
+
+    double finit() const { return finit_; }
+
+    double ginit() const { return ginit_; }
+
+  private:
+    MutRef<ArrayXd> &x() { return *x_; }
+
+    const ArrayXd &t() const { return *t_; }
+
+    const ArrayXd &z() const { return *z_; }
+
+    const ArrayXd &d() const { return *d_; }
+
+    const LbgfsbBounds &bounds() const { return *bounds_; }
+
+    enum class DcsrchStatus : std::uint8_t;
+    DcsrchStatus dcsrch(double f, double g);
+
+    // NOLINTNEXTLINE(readability-identifier-naming)
+    constexpr static double xtrapl_ = 1.1, xtrapu_ = 4, stepmin_ = 0;
+
+    MutRef<ArrayXd> *x_;
+    const ArrayXd *t_, *z_, *d_;
+    const LbgfsbBounds *bounds_;
+    double dtd_, dnorm_;
+
+    double stepmax_;
+    double finit_, ginit_, gtest_;
+    double ftol_, gtol_, xtol_;
+
+    double step_;
+
+    // The variables stx, fx, gx contain the values of the step, function,
+    // and derivative at the best step.
+    // The variables sty, fy, gy contain the value of the step, function,
+    // and derivative at sty.
+    // The variables stp, f, g contain the values of the step, function,
+    // and derivative at stp.
+    double stx_ = 0, fx_, gx_;
+    double sty_ = 0, fy_, gy_;
+    double stmin_ = 0, stmax_;
+    double width_, width1_;
+
+    bool brackt_ = false, stage1_ = true;
   };
 
   extern std::pair<bool, bool> lbfgsb_active(MutRef<ArrayXd> &x,
@@ -115,7 +182,135 @@ namespace internal {
                            const ArrayXd &gg, ConstRef<MatrixXd> ws,
                            ConstRef<MatrixXd> wy, const LbgfsbBounds &bounds,
                            double theta);
+
+  extern bool lbfgsb_prepare_lnsrch(
+      MutRef<ArrayXd> x, ArrayXd &z, ArrayXd &xp, ArrayXd &r, ArrayXd &d,
+      ArrayXi &iwhere, MatrixXd &wnt_d, MatrixXd &wn1, MatrixXd &ws_d,
+      MatrixXd &wy_d, MatrixXd &sy_d, MatrixXd &wtt_d, VectorXd &p_d,
+      VectorXd &v_d, VectorXd &c_d, VectorXd &wbp_d, ArrayXi &free_bound,
+      int &nfree, ArrayXi &enter_leave, int &nenter, int &nleave,
+      ClearablePQ<CauchyBrkpt, std::greater<>> &brks, Eigen::LLT<MatrixXd> &llt,
+      const ArrayXd &gx, const LbgfsbBounds &bounds, double sbgnrm,
+      double theta, bool updated, bool constrained, int iter, int col);
+
+  extern bool lbfgsb_prepare_next_iter(ArrayXd &r, ArrayXd &d, MatrixXd &ws_d,
+                                       MatrixXd &wy_d, MatrixXd &ss_d,
+                                       MatrixXd &sy_d, MatrixXd &wtt_d,
+                                       MatrixXd &wnt_d, double &theta, int &col,
+                                       bool &updated, Eigen::LLT<MatrixXd> &llt,
+                                       double gd, double ginit, double step,
+                                       double dtd);
+
+  template <class FuncGrad>
+  bool lbfgsb_main(FuncGrad fg, MutRef<ArrayXd> x, const LbgfsbBounds &bounds,
+                   const int m, const double factr, const int maxiter,
+                   const int maxls, const double pgtol) {
+    const auto n = x.size();
+    const double tol = factr * kEpsMach;
+
+    ArrayXi iwhere = ArrayXi::Zero(n);
+    auto [constrained, boxed] = lbfgsb_active(x, iwhere, bounds);
+
+    ArrayXd gx(n);
+    double fx = fg(gx, x);
+    double sbgnrm = lbfgsb_projgr(x, gx, bounds);
+    if (sbgnrm <= pgtol)
+      return true;
+
+    MatrixXd wnt(2 * m, 2 * m), wn1(2 * m, 2 * m);
+    MatrixXd ws(n, m), wy(n, m);
+    MatrixXd ss(m, m), sy(m, m), wtt(m, m);
+    VectorXd p(2 * m), v(2 * m), c(2 * m), wbp(3 * m - 1);
+    ArrayXd z(n), r(n), t(n), d(n);
+    Eigen::LLT<MatrixXd> llt(1);
+
+    ArrayXi free_bound(n), enter_leave(n);
+    ClearablePQ<CauchyBrkpt, std::greater<>> brks;
+    brks.data().reserve(n);
+
+    int nfree = static_cast<int>(n), nenter = 0, nleave = 0, col = 0;
+    double theta = 1;
+    bool updated = false;
+
+    auto reset_memory = [&]() {
+      col = 0;
+      theta = 1.0;
+      updated = false;
+    };
+
+    for (int iter = 0; iter < maxiter; ++iter) {
+      if (!lbfgsb_prepare_lnsrch(
+              x, z, t, r, d, iwhere, wnt, wn1, ws, wy, sy, wtt, p, v, c, wbp,
+              free_bound, nfree, enter_leave, nenter, nleave, brks, llt, gx,
+              bounds, sbgnrm, theta, updated, constrained, iter, col)) {
+        reset_memory();
+        continue;
+      }
+
+      d = z - x;
+      t = x;
+      r = gx;
+
+      double gd = gx.matrix().dot(d.matrix());
+      LbfgsbLnsrch lnsrlb(x, t, z, d, bounds, fx, gd, iter, constrained, boxed);
+      bool converged = false;
+      for (int i = 0; i < maxls; ++i) {
+        if (gd >= 0)
+          break;
+
+        converged = lnsrlb.search(fx, gd);
+        if (converged)
+          break;
+
+        fx = fg(gx, x);
+        gd = gx.matrix().dot(d.matrix());
+      }
+
+      if (!converged) {
+        x = t;
+        fx = lnsrlb.finit();
+        gx = r;
+
+        // Abnormal termination
+        if (col == 0)
+          return false;
+
+        reset_memory();
+        continue;
+      }
+
+      sbgnrm = lbfgsb_projgr(x, gx, bounds);
+      if (sbgnrm <= pgtol)
+        return true;
+
+      double ddum = std::max({ std::abs(lnsrlb.finit()), std::abs(fx), 1.0 });
+      if (lnsrlb.finit() - fx <= tol * ddum)
+        return true;
+
+      r = gx - r;
+      if (!lbfgsb_prepare_next_iter(r, d, ws, wy, ss, sy, wtt, wnt, theta, col,
+                                    updated, llt, gd, lnsrlb.ginit(),
+                                    lnsrlb.step(), lnsrlb.dtd())) {
+        reset_memory();
+      }
+    }
+
+    return false;
+  }
 }  // namespace internal
+
+template <class FuncGrad>
+bool l_bfgs_b(FuncGrad &&fg, MutRef<ArrayXd> x, const ArrayXi &nbd,
+              const Array2Xd &bounds, const int m, const double factr,
+              const int maxiter, const int maxls, const double pgtol) {
+  bool args_ok = internal::lbfgsb_errclb(x, nbd, bounds, m, factr);
+  if (!args_ok)
+    return false;
+
+  internal::LbgfsbBounds bds(nbd, bounds);
+  return internal::lbfgsb_main(std::forward<FuncGrad>(fg), x, bds, m, factr,
+                               maxiter, maxls, pgtol);
+}
 }  // namespace nuri
 
 #endif /* NURI_ALGO_OPTIM_H_ */
--- src/algo/optim.cpp
+++ src/algo/optim.cpp
@@ -18,6 +18,23 @@
 
 namespace nuri {
 namespace internal {
+  bool lbfgsb_errclb(const ArrayXd &x, const ArrayXi &nbd,
+                     const Array2Xd &bounds, const int m, const double factr) {
+    if (x.size() <= 0)
+      return false;
+    if (m <= 0)
+      return false;
+    if (factr < 0.0)
+      return false;
+
+    if (x.size() != nbd.size() || ((nbd < 0) || (nbd > 3)).any())
+      return false;
+    if (x.size() != bounds.cols() || (bounds.row(0) > bounds.row(1)).any())
+      return false;
+
+    return true;
+  }
+
   std::pair<bool, bool> lbfgsb_active(MutRef<ArrayXd> &x, ArrayXi &iwhere,
                                       const LbgfsbBounds &bounds) {
     bool constrained = false, boxed = false;
@@ -266,6 +283,43 @@ namespace internal {
   }
 
   namespace {
+    bool lbfgsb_freev(ArrayXi &free_bound, int &nfree, ArrayXi &enter_leave,
+                      int &nenter, int &nleave, const ArrayXi &iwhere,
+                      const int iter, const bool constrained,
+                      const bool updated) {
+      const auto n = free_bound.size();
+
+      if (iter > 0 && constrained) {
+        nenter = nleave = 0;
+
+        for (int i = 0; i < nfree; ++i) {
+          int k = free_bound[i];
+          if (iwhere[k] > 0)
+            enter_leave[n - ++nleave] = k;
+        }
+
+        for (int i = nfree; i < free_bound.size(); ++i) {
+          int k = free_bound[i];
+          if (iwhere[k] <= 0)
+            enter_leave[nenter++] = k;
+        }
+      }
+
+      nfree = 0;
+      auto iact = free_bound.size();
+      for (int i = 0; i < free_bound.size(); ++i) {
+        if (iwhere[i] <= 0) {
+          free_bound[nfree] = i;
+          ++nfree;
+        } else {
+          --iact;
+          free_bound[iact] = i;
+        }
+      }
+
+      return nenter + nleave > 0 || updated;
+    }
+
     void formk_shift_wn1(MatrixXd &wn1) {
       const auto m = wn1.cols() / 2, mm1 = m - 1;
 
@@ -553,6 +607,466 @@ namespace internal {
     x(free) += alpha * d.array();
 
     return true;
+  }
+
+  namespace {
+    void dcstep(double &stx, double &fx, double &dx, double &sty, double &fy,
+                double &dy, double &stp, bool &brackt, const double fp,
+                const double dp, const double stpmin, const double stpmax) {
+      const double sgnd = dp * dx;
+
+      auto theta_gamma_common = [&]() {
+        double theta = 3 * (fx - fp) / (stp - stx) + dx + dp;
+        double gamma = std::sqrt(nonnegative(theta * theta - dx * dp));
+        if (stp < stx)
+          gamma = -gamma;
+        return std::make_pair(theta, gamma);
+      };
+
+      auto update_intervals = [&](double new_step) {
+        if (fp > fx) {
+          sty = stp;
+          fy = fp;
+          dy = dp;
+        } else {
+          if (sgnd < 0) {
+            sty = stx;
+            fy = fx;
+            dy = dx;
+          }
+          stx = stp;
+          fx = fp;
+          dx = dp;
+        }
+
+        stp = new_step;
+      };
+
+      // First case: A higher function value. The minimum is bracketed.
+      // If the cubic step is closer to stx than the quadratic step, the
+      // cubic step is taken, otherwise the average of the cubic and
+      // quadratic steps is taken.
+      if (fp > fx) {
+        auto [theta, gamma] = theta_gamma_common();
+        double p = (gamma - dx) + theta;
+        double q = ((gamma - dx) + gamma) + dp;
+        double r = p / q;
+
+        double stpc = stx + r * (stp - stx);
+        double stpq =
+            stx + dx / ((fx - fp) / (stp - stx) + dx) / 2 * (stp - stx);
+
+        brackt = true;
+        if (std::abs(stpc - stx) < std::abs(stpq - stx)) {
+          update_intervals(stpc);
+        } else {
+          update_intervals(stpc + (stpq - stpc) / 2);
+        }
+
+        return;
+      }
+
+      // Second case: A lower function value and derivatives of opposite
+      // sign. The minimum is bracketed. If the cubic step is farther from
+      // stp than the secant step, the cubic step is taken, otherwise the
+      // secant step is taken.
+      if (sgnd < 0.0) {
+        auto [theta, gamma] = theta_gamma_common();
+        double p = (gamma - dp) + theta;
+        double q = ((gamma - dp) + gamma) + dx;
+        double r = p / q;
+
+        double stpc = stp + r * (stx - stp);
+        double stpq = stp + dp / (dp - dx) * (stx - stp);
+
+        brackt = true;
+        if (std::abs(stpc - stp) > std::abs(stpq - stp)) {
+          update_intervals(stpc);
+        } else {
+          update_intervals(stpq);
+        }
+
+        return;
+      }
+
+      // Third case: A lower function value, derivatives of the same sign,
+      // and the magnitude of the derivative decreases.
+
+      // The cubic step is computed only if the cubic tends to infinity
+      // in the direction of the step or if the minimum of the cubic
+      // is beyond stp. Otherwise the cubic step is defined to be the
+      // secant step.
+      if (std::abs(dp) < std::abs(dx)) {
+        auto [theta, gamma] = theta_gamma_common();
+        double p = (gamma - dp) + theta;
+        double q = (gamma + (dx - dp)) + gamma;
+        double r = p / q;
+
+        double stpc;
+        if (r < 0 && gamma != 0) {  // NOLINT(clang-diagnostic-float-equal)
+          stpc = stp + r * (stx - stp);
+        } else if (stp > stx) {
+          stpc = stpmax;
+        } else {
+          stpc = stpmin;
+        }
+        double stpq = stp + dp / (dp - dx) * (stx - stp);
+
+        double stpf;
+        if (brackt) {
+          // A minimizer has been bracketed. If the cubic step is
+          // closer to stp than the secant step, the cubic step is
+          // taken, otherwise the secant step is taken.
+          if (std::abs(stpc - stp) < std::abs(stpq - stp)) {
+            stpf = stpc;
+          } else {
+            stpf = stpq;
+          }
+          if (stp > stx) {
+            stpf = nuri::min(stp + 0.66 * (sty - stp), stpf);
+          } else {
+            stpf = nuri::max(stp + 0.66 * (sty - stp), stpf);
+          }
+        } else {
+          // A minimizer has not been bracketed. If the cubic step is
+          // farther from stp than the secant step, the cubic step is
+          // taken, otherwise the secant step is taken.
+          if (std::abs(stpc - stp) > std::abs(stpq - stp)) {
+            stpf = stpc;
+          } else {
+            stpf = stpq;
+          }
+          stpf = nuri::clamp(stpf, stpmin, stpmax);
+        }
+        update_intervals(stpf);
+
+        return;
+      }
+
+      // Fourth case: A lower function value, derivatives of the same sign,
+      // and the magnitude of the derivative does not decrease. If the
+      // minimum is not bracketed, the step is either stpmin or stpmax,
+      // otherwise the cubic step is taken.
+      if (brackt) {
+        double theta = 3 * (fp - fy) / (sty - stp) + dy + dp;
+        double gamma = std::sqrt(nonnegative(theta * theta - dy * dp));
+        if (stp > sty)
+          gamma = -gamma;
+
+        double p = (gamma - dp) + theta;
+        double q = ((gamma - dp) + gamma) + dy;
+        double r = p / q;
+        double stpc = stp + r * (sty - stp);
+        update_intervals(stpc);
+      } else if (stp > stx) {
+        update_intervals(stpmax);
+      } else {
+        update_intervals(stpmin);
+      }
+    }
+  }  // namespace
+
+  enum class LbfgsbLnsrch::DcsrchStatus : std::uint8_t {
+    kFound,
+    kConverged,
+    kContinue,
+  };
+
+  LbfgsbLnsrch::LbfgsbLnsrch(MutRef<ArrayXd> &x, const ArrayXd &t,
+                             const ArrayXd &z, const ArrayXd &d,
+                             const LbgfsbBounds &bounds, const double f0,
+                             const double g0, const int iter,
+                             const bool constrained, const bool boxed,
+                             const double ftol, const double gtol,
+                             const double xtol) noexcept
+      : x_(&x), t_(&t), z_(&z), d_(&d), bounds_(&bounds),
+        dtd_(d.matrix().squaredNorm()), dnorm_(std::sqrt(dtd_)), finit_(f0),
+        ginit_(g0), gtest_(ftol * ginit_), ftol_(ftol), gtol_(gtol),
+        xtol_(xtol), fx_(f0), gx_(g0), fy_(f0), gy_(g0) {
+    const auto n = x.size();
+
+    if (constrained && iter == 0) {
+      stepmax_ = 1;
+    } else if (constrained) {
+      stepmax_ = 1e+10;
+      for (int i = 0; i < n; ++i) {
+        if (!bounds.has_bound(i))
+          continue;
+
+        double a1 = d[i];
+        if (a1 < 0 && bounds.has_lb(i)) {
+          double a2 = bounds.lb(i) - x[i];
+          if (a2 >= 0) {
+            stepmax_ = 0;
+            break;
+          }
+          if (a1 * stepmax_ < a2)
+            stepmax_ = a2 / a1;
+        } else if (a1 > 0 && bounds.has_ub(i)) {
+          double a2 = bounds.ub(i) - x[i];
+          if (a2 <= 0) {
+            stepmax_ = 0;
+            break;
+          }
+          if (a1 * stepmax_ > a2)
+            stepmax_ = a2 / a1;
+        }
+      }
+    }
+
+    if (iter == 0 && !boxed) {
+      step_ = nuri::min(1 / dnorm_, stepmax_);
+    } else {
+      step_ = 1;
+    }
+
+    // init for dcsrch
+    width_ = stepmax_ - stepmin_;
+    width1_ = 2 * width_;
+    stmax_ = step_ + xtrapu_ * step_;
+  }
+
+  bool LbfgsbLnsrch::search(const double f, const double g) {
+    auto status = dcsrch(f, g);
+    if (status == DcsrchStatus::kConverged)
+      return true;
+
+    if (step_ == 1) {  // NOLINT(clang-diagnostic-float-equal)
+      x() = z();
+      return status == DcsrchStatus::kFound;
+    }
+
+    x() = step_ * d() + t();
+    for (int i = 0; i < x().size(); ++i) {
+      if (bounds().has_lb(i)) {
+        x()[i] = nuri::max(bounds().lb(i), x()[i]);
+      }
+      if (bounds().has_ub(i)) {
+        x()[i] = nuri::min(bounds().ub(i), x()[i]);
+      }
+    }
+
+    return status == DcsrchStatus::kFound;
+  }
+
+  LbfgsbLnsrch::DcsrchStatus LbfgsbLnsrch::dcsrch(const double f,
+                                                  const double g) {
+    double ftest = finit() + step_ * gtest_;
+
+    // Termination check conditions
+    if ((step_ >= stepmax_ && f <= ftest && g <= gtest_)
+        || (step_ <= stepmin_ && (f > ftest || g >= gtest_)))
+      return DcsrchStatus::kConverged;
+    if (f <= ftest && std::abs(g) <= -gtol_ * ginit())
+      return DcsrchStatus::kConverged;
+
+    // If psi(stp) <= 0 and f'(stp) >= 0 for some step, then the algorithm
+    // enters the second stage.
+    if (stage1_ && f <= ftest && g >= 0)
+      stage1_ = false;
+
+    // A modified function is used to predict the step during the first stage
+    // if a lower function value has been obtained but the decrease is not
+    // sufficient.
+
+    if (stage1_ && f <= fx_ && f > ftest) {
+      double fm = f - step_ * gtest_;
+      double fxm = fx_ - stx_ * gtest_;
+      double fym = fy_ - sty_ * gtest_;
+      double gm = g - gtest_;
+      double gxm = gx_ - gtest_;
+      double gym = gy_ - gtest_;
+
+      dcstep(stx_, fxm, gxm, sty_, fym, gym, step_, brackt_, fm, gm, stmin_,
+             stmax_);
+
+      fx_ = fxm + stx_ * gtest_;
+      fy_ = fym + sty_ * gtest_;
+      gx_ = gxm + gtest_;
+      gy_ = gym + gtest_;
+    } else {
+      dcstep(stx_, fx_, gx_, sty_, fy_, gy_, step_, brackt_, f, g, stmin_,
+             stmax_);
+    }
+
+    // Decide if a bisection step is needed.
+    if (brackt_) {
+      if (std::abs(sty_ - stx_) >= 0.66 * width1_)
+        step_ = stx_ + 0.5 * (sty_ - stx_);
+      width1_ = width_;
+      width_ = std::abs(sty_ - stx_);
+    }
+
+    // Set the minimum and maximum steps allowed for stp.
+    if (brackt_) {
+      std::tie(stmin_, stmax_) = nuri::minmax(stx_, sty_);
+    } else {
+      stmin_ = step_ * xtrapl_ * (step_ - stx_);
+      stmax_ = step_ + xtrapu_ * (step_ - stx_);
+    }
+
+    step_ = nuri::clamp(step_, stepmin_, stepmax_);
+
+    // If further progress is not possible, let stp be the best point obtained
+    // during the search.
+
+    if (brackt_
+        && (step_ <= stmin_ || step_ >= stmax_
+            || stmax_ - stmin_ <= xtol_ * stmax_)) {
+      step_ = stx_;
+      return DcsrchStatus::kFound;
+    }
+
+    return DcsrchStatus::kContinue;
+  }
+
+  bool lbfgsb_prepare_lnsrch(
+      MutRef<ArrayXd> x, ArrayXd &z, ArrayXd &xp, ArrayXd &r, ArrayXd &d,
+      ArrayXi &iwhere, MatrixXd &wnt_d, MatrixXd &wn1, MatrixXd &ws_d,
+      MatrixXd &wy_d, MatrixXd &sy_d, MatrixXd &wtt_d, VectorXd &p_d,
+      VectorXd &v_d, VectorXd &c_d, VectorXd &wbp_d, ArrayXi &free_bound,
+      int &nfree, ArrayXi &enter_leave, int &nenter, int &nleave,
+      ClearablePQ<CauchyBrkpt, std::greater<>> &brks, Eigen::LLT<MatrixXd> &llt,
+      const ArrayXd &gx, const LbgfsbBounds &bounds, const double sbgnrm,
+      const double theta, const bool updated, const bool constrained,
+      const int iter, const int col) {
+    const auto n = x.size();
+
+    MutRef<MatrixXd> wnt(wnt_d.topLeftCorner(2 * col, 2 * col));
+    MutRef<MatrixXd> ws(ws_d.leftCols(col)), wy(wy_d.leftCols(col));
+    MutRef<MatrixXd> sy(sy_d.topLeftCorner(col, col)),
+        wtt(wtt_d.topLeftCorner(col, col));
+    MutRef<VectorXd> p(p_d.head(2 * col)), v(v_d.head(2 * col)),
+        c(c_d.head(2 * col)), wbp = wbp_d.head(2 * col);
+
+    MutRef<ArrayXd> smul = wbp_d.tail(col - 1).array();
+
+    bool need_k;
+    if (!constrained && col > 0) {
+      z = x;
+      need_k = updated;
+    } else {
+      if (!lbfgsb_cauchy(z, iwhere, p, v, c, d, wbp, smul, brks, x, gx, bounds,
+                         ws, wy, sy, wtt, sbgnrm, theta))
+        return false;
+
+      need_k = lbfgsb_freev(free_bound, nfree, enter_leave, nenter, nleave,
+                            iwhere, iter, constrained, updated);
+    }
+
+    if (nfree > 0 && col > 0) {
+      if (need_k) {
+        if (!lbfgsb_formk(wnt, wn1, llt, free_bound.head(nfree),
+                          free_bound.tail(n - nfree), enter_leave.head(nenter),
+                          enter_leave.tail(nleave), ws, wy, sy, theta, updated))
+          return false;
+      }
+
+      MutRef<VectorXd> rfree(r.head(nfree).matrix());
+      if (!lbfgsb_cmprlb(rfree, p, smul, c, free_bound.head(nfree), x, z, gx,
+                         ws, wy, sy, wtt, theta, constrained))
+        return false;
+      if (!lbfgsb_subsm(z, xp, rfree, p, wnt, free_bound.head(nfree), x, gx, ws,
+                        wy, bounds, theta))
+        return false;
+    }
+
+    return true;
+  }
+
+  namespace {
+    int lbfgsb_matupd(MatrixXd &ws, MatrixXd &wy, MatrixXd &ss, MatrixXd &sy,
+                      const ArrayXd &d, const ArrayXd &r, const double dr,
+                      const double step, const double dtd, const int pcol) {
+      const int m = static_cast<int>(ws.cols());
+      int col, ci;
+
+      if (pcol < m) {
+        col = pcol + 1;
+        ci = pcol;
+      } else {
+        ws.leftCols(m - 1) = ws.rightCols(m - 1);
+        wy.leftCols(m - 1) = wy.rightCols(m - 1);
+        col = m;
+        ci = m - 1;
+      }
+
+      ws.col(ci) = d;
+      wy.col(ci) = r;
+
+      // Form the middle matrix in B.
+      // update the upper triangle of SS (m,m), and the lower triangle of SY
+      // (m,m):
+      ss.topLeftCorner(pcol - 1, pcol - 1).triangularView<Eigen::Upper>() =
+          ss.block(1, 1, pcol - 1, pcol - 1).triangularView<Eigen::Upper>();
+      sy.topLeftCorner(pcol - 1, pcol - 1).triangularView<Eigen::Lower>() =
+          sy.block(1, 1, pcol - 1, pcol - 1).triangularView<Eigen::Lower>();
+
+      ss.col(ci).head(col) = ws.leftCols(col).transpose() * d.matrix();
+      sy.row(ci).head(col) = d.transpose().matrix() * wy.leftCols(col);
+
+      ss(ci, ci) = step * step * dtd;
+      sy(ci, ci) = dr;
+
+      return col;
+    }
+
+    bool lbfgsb_formt(MutRef<MatrixXd> wtt, Eigen::LLT<MatrixXd> &llt,
+                      MutRef<MatrixXd> sy_scaled, ConstRef<MatrixXd> sy,
+                      ConstRef<MatrixXd> ss, const double theta) {
+      const auto col = ss.cols();
+
+      // Form the upper half of  T = theta*SS + L*D^(-1)*L', store T in the
+      // upper triangle of the array wt.
+      wtt.triangularView<Eigen::Lower>() =
+          (theta * ss.transpose()).triangularView<Eigen::Lower>();
+
+      sy_scaled.array() = sy.array().colwise() / sy.diagonal().array();
+      for (int i = 1; i < col; ++i) {
+        wtt.col(i).tail(col - i) += sy.bottomLeftCorner(col - i, i)
+                                    * sy_scaled.row(i).head(i).transpose();
+      }
+
+      llt.compute(wtt);
+      if (llt.info() != Eigen::Success)
+        return false;
+
+      wtt.triangularView<Eigen::Lower>() = llt.matrixL();
+      return true;
+    }
+  }  // namespace
+
+  bool lbfgsb_prepare_next_iter(ArrayXd &r, ArrayXd &d, MatrixXd &ws_d,
+                                MatrixXd &wy_d, MatrixXd &ss_d, MatrixXd &sy_d,
+                                MatrixXd &wtt_d, MatrixXd &wnt_d, double &theta,
+                                int &col, bool &updated,
+                                Eigen::LLT<MatrixXd> &llt, const double gd,
+                                const double ginit, const double step,
+                                const double dtd) {
+    double rr = r.matrix().squaredNorm(), dr, ddum;
+    if (step == 1) {  // NOLINT(clang-diagnostic-float-equal)
+      dr = gd - ginit;
+      ddum = -ginit;
+    } else {
+      dr = (gd - ginit) * step;
+      d *= step;
+      ddum = -ginit * step;
+    }
+
+    if (dr <= kEpsMach * ddum) {
+      updated = false;
+      return true;
+    }
+
+    updated = true;
+    theta = rr / dr;
+    col = lbfgsb_matupd(ws_d, wy_d, ss_d, sy_d, d, r, dr, step, dtd, col);
+    // wn will be recalculated in the next iteration, use for temporary
+    // storage
+    return lbfgsb_formt(wtt_d.topLeftCorner(col, col), llt,
+                        wnt_d.topLeftCorner(col, col),
+                        sy_d.topLeftCorner(col, col),
+                        ss_d.topLeftCorner(col, col), theta);
   }
 }  // namespace internal
 }  // namespace nuri
