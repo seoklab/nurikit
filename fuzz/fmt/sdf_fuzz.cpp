95c6af8bd538fd1ff23dfec468fe5796a6807d8d
Nuri Jung <jnooree@snu.ac.kr>
2024-12-23 22:46:46 +0900

    test(fmt/sdf): fuzz sdf reader/writer

---
+++ fuzz/fmt/sdf_fuzz.cpp
@@ -0,0 +1,42 @@
+//
+// Project NuriKit - Copyright 2024 SNU Compbio Lab.
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <sstream>
+#include <vector>
+
+#include <absl/base/call_once.h>
+#include <absl/base/log_severity.h>
+#include <absl/log/globals.h>
+#include <absl/log/initialize.h>
+
+#include "fuzz_utils.h"
+#include "nuri/core/molecule.h"
+#include "nuri/fmt/sdf.h"
+
+NURI_FUZZ_MAIN(data, size) {
+  static absl::once_flag flag;
+  absl::call_once(flag, []() {
+    absl::InitializeLog();
+    absl::SetStderrThreshold(absl::LogSeverity::kFatal);
+  });
+
+  std::istringstream iss(
+      std::string { reinterpret_cast<const char *>(data), size });
+  nuri::SDFReader reader(iss);
+
+  std::vector<std::string> block;
+  std::string sdf;
+  while (reader.getnext(block)) {
+    nuri::Molecule mol = reader.parse(block);
+    if (mol.empty())
+      continue;
+
+    nuri::write_sdf(sdf, mol, -1, nuri::SDFVersion::kAutomatic);
+    nuri::write_sdf(sdf, mol, -1, nuri::SDFVersion::kV2000);
+    nuri::write_sdf(sdf, mol, -1, nuri::SDFVersion::kV3000);
+  }
+
+  return 0;
+}
