7cc227b4ac1cdfc6e1923c4c6324e42d623e30e2
Nuri Jung <jnooree@snu.ac.kr>
2024-12-24 11:33:25 +0900

    feat(scripts/fuzz): make fuzzer script configurable (#442)
    [skip ci]

--- scripts/fuzz.sh
+++ scripts/fuzz.sh
@@ -6,31 +6,58 @@
 
 set -euo pipefail
 
-type clang++ llvm-profdata llvm-cov c++filt
-
 project_root="$(dirname "$(dirname "$(realpath "$0")")")"
 suppression="$(printf '%q' "$project_root/.github/ubsanignore.txt")"
 
+coverage=false
+corpus=corpus
+while getopts ':-:' opt; do
+	case "$opt" in
+	-) ;;
+	*) exit 1 ;;
+	esac
+
+	case "$OPTARG" in
+	coverage) coverage=true ;;
+	corpus)
+		corpus="${!OPTIND}"
+		((++OPTIND))
+		;;
+	*)
+		echo >&2 "Unknown option: --$OPTARG"
+		exit 1
+		;;
+	esac
+done
+
+shift $((OPTIND - 1))
+
 exe="$(realpath "$1")"
 build_dir="$(dirname "$(dirname "$exe")")"
-corpus="$(realpath "${2-corpus}")"
 
-shift 2
+shift 1
+
+type clang++
+if [[ $coverage == true ]]; then
+	type llvm-profdata llvm-cov c++filt
+fi
 
 LLVM_PROFILE_FILE='coverage.profraw' \
 	LD_PRELOAD="$(clang++ -print-file-name=libclang_rt.asan-x86_64.so) /usr/lib/x86_64-linux-gnu/libubsan.so.1" \
 	UBSAN_OPTIONS="suppressions=$suppression print_stacktrace=1" \
-	"$exe" "$corpus" "$@"
+	echo -- "$exe" "$corpus" "$@"
 
-llvm-profdata merge -sparse coverage.profraw -o coverage.profdata
+if [[ $coverage == true ]]; then
+	llvm-profdata merge -sparse coverage.profraw -o coverage.profdata
 
-llvm-cov show -format html -output-dir cov-html -Xdemangler c++filt \
-	-instr-profile coverage.profdata \
-	"$exe" \
-	-object "$build_dir/lib/libnuri.so"
-chmod -R go+rX cov-html
+	llvm-cov show -format html -output-dir cov-html -Xdemangler c++filt \
+		-instr-profile coverage.profdata \
+		"$exe" \
+		-object "$build_dir/lib/libnuri.so"
+	chmod -R go+rX cov-html
 
-llvm-cov report \
-	-instr-profile coverage.profdata \
-	"$exe" \
-	-object "$build_dir/lib/libnuri.so"
+	llvm-cov report \
+		-instr-profile coverage.profdata \
+		"$exe" \
+		-object "$build_dir/lib/libnuri.so"
+fi
