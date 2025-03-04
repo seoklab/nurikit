dd5ce102291c76c548fef7484cebf60af336378e
Nuri Jung <jnooree@snu.ac.kr>
2025-03-04 11:20:30 +0900

    test(fmt/pdb): add proofreading tests

--- test/fmt/pdb_test.cpp
+++ test/fmt/pdb_test.cpp
@@ -7,10 +7,12 @@
 
 #include <fstream>
 #include <sstream>
+#include <string>
 #include <vector>
 
 #include <absl/algorithm/container.h>
 #include <absl/strings/match.h>
+#include <absl/strings/str_split.h>
 
 #include <gtest/gtest.h>
 
@@ -32,126 +34,223 @@ public:
   }
 };
 
+std::vector<Molecule> recovered(const Molecule &mol) {
+  std::string pdb;
+  EXPECT_GE(write_pdb(pdb, mol), 0);
+
+  std::istringstream pdbs(pdb);
+  PDBReader reader(pdbs);
+  MoleculeStream<PDBReader> ms(reader);
+
+  std::vector<Molecule> mols;
+  while (ms.advance())
+    mols.push_back(std::move(ms.current()));
+  return mols;
+}
+
 TEST_F(PDBTest, BasicParsing) {
   set_test_file("dummy.pdb");
+  {
+    ASSERT_TRUE(advance_and_guess());
 
-  ASSERT_TRUE(advance_and_guess());
+    EXPECT_EQ(mol().num_atoms(), 88);
+    EXPECT_EQ(mol().num_bonds(), 87);
 
-  EXPECT_EQ(mol().num_atoms(), 88);
-  EXPECT_EQ(mol().num_bonds(), 87);
+    // Peptide + H2O
+    EXPECT_EQ(mol().num_fragments(), 2);
+    // Ring from PHE
+    EXPECT_EQ(mol().ring_groups().size(), 1);
 
-  // Peptide + H2O
-  EXPECT_EQ(mol().num_fragments(), 2);
-  // Ring from PHE
-  EXPECT_EQ(mol().ring_groups().size(), 1);
+    // 11 residues + 1 chain
+    EXPECT_EQ(mol().num_substructures(), 12);
 
-  // 11 residues + 1 chain
-  EXPECT_EQ(mol().num_substructures(), 12);
+    // First residue is MET
+    const Substructure &sub = mol().get_substructure(0);
+    EXPECT_EQ(sub.name(), "MET");
+    EXPECT_EQ(sub.id(), 1);
+    EXPECT_EQ(sub.num_atoms(), 8);
 
-  // First residue is MET
-  const Substructure &sub = mol().get_substructure(0);
-  EXPECT_EQ(sub.name(), "MET");
-  EXPECT_EQ(sub.id(), 1);
-  EXPECT_EQ(sub.num_atoms(), 8);
+    // Phenylalanine N
+    auto atom = mol().atom(25);
+    EXPECT_EQ(atom.data().get_name(), "N");
+    EXPECT_EQ(atom.data().atomic_number(), 7);
+    EXPECT_EQ(atom.data().hybridization(), constants::kSP2);
+    EXPECT_TRUE(atom.data().is_conjugated());
+  }
+  {
+    std::vector mols = recovered(mol());
+    ASSERT_EQ(mols.size(), 1);
 
-  // Phenylalanine N
-  auto atom = mol().atom(25);
-  EXPECT_EQ(atom.data().get_name(), "N");
-  EXPECT_EQ(atom.data().atomic_number(), 7);
-  EXPECT_EQ(atom.data().hybridization(), constants::kSP2);
-  EXPECT_TRUE(atom.data().is_conjugated());
+    EXPECT_EQ(mols[0].num_atoms(), 88);
+    EXPECT_EQ(mols[0].num_substructures(), 12);
+
+    const Substructure &sub = mols[0].get_substructure(0);
+    EXPECT_EQ(sub.name(), "MET");
+    EXPECT_EQ(sub.id(), 1);
+    EXPECT_EQ(sub.num_atoms(), 8);
+
+    auto atom = mols[0].atom(25);
+    EXPECT_EQ(atom.data().get_name(), "N");
+    EXPECT_EQ(atom.data().atomic_number(), 7);
+  }
 }
 
 TEST_F(PDBTest, HandleInvalidCoordinates) {
   set_test_file("invalid.pdb");
+  {
+    ASSERT_TRUE(advance());
+    EXPECT_NO_FATAL_FAILURE(
+        static_cast<void>(internal::guess_update_subs(mol())));
 
-  ASSERT_TRUE(advance());
-  EXPECT_NO_FATAL_FAILURE(
-      static_cast<void>(internal::guess_update_subs(mol())));
+    EXPECT_EQ(mol().num_atoms(), 90);
 
-  EXPECT_EQ(mol().num_atoms(), 90);
+    // 11 residues + 1 chain
+    EXPECT_EQ(mol().num_substructures(), 12);
 
-  // 11 residues + 1 chain
-  EXPECT_EQ(mol().num_substructures(), 12);
+    // First residue is MET
+    const Substructure &sub = mol().get_substructure(0);
+    EXPECT_EQ(sub.name(), "MET");
+    EXPECT_EQ(sub.id(), 1);
+    EXPECT_EQ(sub.num_atoms(), 8);
 
-  // First residue is MET
-  const Substructure &sub = mol().get_substructure(0);
-  EXPECT_EQ(sub.name(), "MET");
-  EXPECT_EQ(sub.id(), 1);
-  EXPECT_EQ(sub.num_atoms(), 8);
+    // Phenylalanine N
+    auto atom = mol().atom(25);
+    EXPECT_EQ(atom.data().get_name(), "N");
+    EXPECT_EQ(atom.data().atomic_number(), 7);
+  }
+  {
+    std::vector mols = recovered(mol());
+    ASSERT_EQ(mols.size(), 1);
 
-  // Phenylalanine N
-  auto atom = mol().atom(25);
-  EXPECT_EQ(atom.data().get_name(), "N");
-  EXPECT_EQ(atom.data().atomic_number(), 7);
+    EXPECT_EQ(mols[0].num_atoms(), 90);
+    EXPECT_EQ(mols[0].num_substructures(), 12);
+
+    const Substructure &sub = mols[0].get_substructure(0);
+    EXPECT_EQ(sub.name(), "MET");
+    EXPECT_EQ(sub.id(), 1);
+    EXPECT_EQ(sub.num_atoms(), 8);
+
+    auto atom = mols[0].atom(25);
+    EXPECT_EQ(atom.data().get_name(), "N");
+    EXPECT_EQ(atom.data().atomic_number(), 7);
+  }
 }
 
 TEST_F(PDBTest, HandleCleanPDB) {
   set_test_file("1ubq.pdb");
+  {
+    ASSERT_TRUE(advance_and_guess());
 
-  ASSERT_TRUE(advance_and_guess());
+    EXPECT_EQ(mol().name(), "1UBQ");
+    EXPECT_EQ(mol().num_atoms(), 660);
+    EXPECT_EQ(mol().count_heavy_atoms(), 660);
 
-  EXPECT_EQ(mol().name(), "1UBQ");
-  EXPECT_EQ(mol().num_atoms(), 660);
-  EXPECT_EQ(mol().count_heavy_atoms(), 660);
+    Molecule::Atom atom = mol().atom(0);
+    EXPECT_EQ(atom.data().get_name(), "N");
+    EXPECT_EQ(atom.data().hybridization(), constants::kSP3);
+    EXPECT_EQ(atom.data().implicit_hydrogens(), 2);
+    EXPECT_FALSE(atom.data().is_conjugated());
 
-  Molecule::Atom atom = mol().atom(0);
-  EXPECT_EQ(atom.data().get_name(), "N");
-  EXPECT_EQ(atom.data().hybridization(), constants::kSP3);
-  EXPECT_EQ(atom.data().implicit_hydrogens(), 2);
-  EXPECT_FALSE(atom.data().is_conjugated());
+    atom = mol().atom(2);
+    EXPECT_EQ(atom.data().get_name(), "C");
+    EXPECT_EQ(atom.data().hybridization(), constants::kSP2);
+    EXPECT_TRUE(atom.data().is_conjugated());
 
-  atom = mol().atom(2);
-  EXPECT_EQ(atom.data().get_name(), "C");
-  EXPECT_EQ(atom.data().hybridization(), constants::kSP2);
-  EXPECT_TRUE(atom.data().is_conjugated());
+    ASSERT_EQ(mol().confs().size(), 1);
+    // 134 residues + 1 chains
+    ASSERT_EQ(mol().num_substructures(), 135);
+  }
+  {
+    std::vector mols = recovered(mol());
+    ASSERT_EQ(mols.size(), 1);
 
-  ASSERT_EQ(mol().confs().size(), 1);
-  // 134 residues + 1 chains
-  ASSERT_EQ(mol().num_substructures(), 135);
+    EXPECT_EQ(mols[0].num_atoms(), 660);
+    EXPECT_EQ(mols[0].count_heavy_atoms(), 660);
+
+    Molecule::Atom atom = mols[0].atom(0);
+    EXPECT_EQ(atom.data().get_name(), "N");
+
+    atom = mols[0].atom(2);
+    EXPECT_EQ(atom.data().get_name(), "C");
+
+    ASSERT_EQ(mols[0].confs().size(), 1);
+    // 134 residues + 1 chains
+    ASSERT_EQ(mols[0].num_substructures(), 135);
+  }
 }
 
 TEST_F(PDBTest, HandleMultipleModels) {
   set_test_file("3cye_part.pdb");
+  {
+    ASSERT_TRUE(advance_and_guess());
+    EXPECT_EQ(mol().name(), "3CYE");
+    EXPECT_EQ(internal::get_key(mol().props(), "model"), "1");
 
-  ASSERT_TRUE(advance_and_guess());
-  EXPECT_EQ(mol().name(), "3CYE");
-  EXPECT_EQ(internal::get_key(mol().props(), "model"), "1");
+    EXPECT_EQ(mol().num_atoms(), 67);
+    EXPECT_EQ(mol().num_bonds(), 67);
 
-  EXPECT_EQ(mol().num_atoms(), 67);
-  EXPECT_EQ(mol().num_bonds(), 67);
+    // Disulfide bond
+    EXPECT_NE(mol().find_bond(28, 34), mol().bond_end());
+    EXPECT_EQ(mol().atom(28).data().implicit_hydrogens(), 0);
+    EXPECT_EQ(mol().atom(34).data().implicit_hydrogens(), 0);
 
-  // Disulfide bond
-  EXPECT_NE(mol().find_bond(28, 34), mol().bond_end());
-  EXPECT_EQ(mol().atom(28).data().implicit_hydrogens(), 0);
-  EXPECT_EQ(mol().atom(34).data().implicit_hydrogens(), 0);
+    ASSERT_EQ(mol().confs().size(), 2);
+    NURI_EXPECT_EIGEN_EQ(mol().confs()[0].col(0), mol().confs()[1].col(0));
+    NURI_EXPECT_EIGEN_NE(mol().confs()[0].col(43), mol().confs()[1].col(43));
 
-  ASSERT_EQ(mol().confs().size(), 2);
-  NURI_EXPECT_EIGEN_EQ(mol().confs()[0].col(0), mol().confs()[1].col(0));
-  NURI_EXPECT_EIGEN_NE(mol().confs()[0].col(43), mol().confs()[1].col(43));
+    // 9 residues + 1 chain
+    ASSERT_EQ(mol().num_substructures(), 10);
+    EXPECT_EQ(mol().get_substructure(0).name(), "VAL");
+    EXPECT_EQ(mol().get_substructure(0).num_atoms(), 7);
 
-  // 9 residues + 1 chain
-  ASSERT_EQ(mol().num_substructures(), 10);
-  EXPECT_EQ(mol().get_substructure(0).name(), "VAL");
-  EXPECT_EQ(mol().get_substructure(0).num_atoms(), 7);
+    EXPECT_EQ(internal::get_key(mol().get_substructure(8).props(), "icode"),
+              "A");
+  }
+  {
+    std::vector mols = recovered(mol());
+    ASSERT_EQ(mols.size(), 2);
 
-  EXPECT_EQ(internal::get_key(mol().get_substructure(8).props(), "icode"), "A");
+    for (const Molecule &mol: mols) {
+      EXPECT_EQ(mol.num_atoms(), 67);
 
-  ASSERT_TRUE(advance_and_guess());
-  EXPECT_EQ(mol().name(), "3CYE");
-  EXPECT_EQ(internal::get_key(mol().props(), "model"), "2");
+      ASSERT_EQ(mol.num_substructures(), 10);
+      EXPECT_EQ(mol.get_substructure(0).name(), "VAL");
+      EXPECT_EQ(mol.get_substructure(0).num_atoms(), 7);
 
-  EXPECT_EQ(mol().num_atoms(), 36);
-  EXPECT_EQ(mol().num_bonds(), 34);
+      EXPECT_EQ(internal::get_key(mol.get_substructure(8).props(), "icode"),
+                "A");
+    }
+  }
 
-  ASSERT_EQ(mol().confs().size(), 2);
-  NURI_EXPECT_EIGEN_EQ(mol().confs()[0].col(0), mol().confs()[1].col(0));
-  NURI_EXPECT_EIGEN_NE(mol().confs()[0].col(31), mol().confs()[1].col(31));
+  {
+    ASSERT_TRUE(advance_and_guess());
+    EXPECT_EQ(mol().name(), "3CYE");
+    EXPECT_EQ(internal::get_key(mol().props(), "model"), "2");
 
-  // 5 residues + 1 chain
-  ASSERT_EQ(mol().num_substructures(), 6);
-  EXPECT_EQ(mol().get_substructure(0).name(), "VAL");
-  EXPECT_EQ(mol().get_substructure(0).num_atoms(), 7);
+    EXPECT_EQ(mol().num_atoms(), 36);
+    EXPECT_EQ(mol().num_bonds(), 34);
+
+    ASSERT_EQ(mol().confs().size(), 2);
+    NURI_EXPECT_EIGEN_EQ(mol().confs()[0].col(0), mol().confs()[1].col(0));
+    NURI_EXPECT_EIGEN_NE(mol().confs()[0].col(31), mol().confs()[1].col(31));
+
+    // 5 residues + 1 chain
+    ASSERT_EQ(mol().num_substructures(), 6);
+    EXPECT_EQ(mol().get_substructure(0).name(), "VAL");
+    EXPECT_EQ(mol().get_substructure(0).num_atoms(), 7);
+  }
+  {
+    std::vector mols = recovered(mol());
+    ASSERT_EQ(mols.size(), 2);
+
+    for (const Molecule &mol: mols) {
+      EXPECT_EQ(mol.num_atoms(), 36);
+
+      ASSERT_EQ(mol.num_substructures(), 6);
+      EXPECT_EQ(mol.get_substructure(0).name(), "VAL");
+      EXPECT_EQ(mol.get_substructure(0).num_atoms(), 7);
+    }
+  }
 }
 
 TEST(PDBErrorTest, HandleSyntaxErrors) {
@@ -222,13 +321,19 @@ protected:
     ASSERT_EQ(mol_.num_substructures(), 6);
 
     ASSERT_FALSE(ms.advance());
+
+    mols_ = recovered(mol_);
+    ASSERT_EQ(mols().size(), 3);
   }
 
   static const Molecule &mol() { return mol_; }
 
+  static const std::vector<Molecule> &mols() { return mols_; }
+
 private:
   // NOLINTNEXTLINE(readability-identifier-naming)
   inline static Molecule mol_;
+  inline static std::vector<Molecule> mols_;
 };
 
 TEST_F(PDB1alxTest, HandleMultipleAltlocs) {
@@ -242,19 +347,50 @@ TEST_F(PDB1alxTest, HandleMultipleAltlocs) {
 
   NURI_EXPECT_EIGEN_NE(mol().confs()[0].col(72), mol().confs()[1].col(72));
   NURI_EXPECT_EIGEN_NE(mol().confs()[1].col(72), mol().confs()[2].col(72));
+
+  NURI_EXPECT_EIGEN_EQ(mols()[0].confs()[0].col(0),
+                       mols()[1].confs()[0].col(0));
+  NURI_EXPECT_EIGEN_EQ(mols()[0].confs()[0].col(0),
+                       mols()[2].confs()[0].col(0));
+
+  NURI_EXPECT_EIGEN_NE(mols()[0].confs()[0].col(31),
+                       mols()[1].confs()[0].col(31));
+  NURI_EXPECT_EIGEN_EQ(mols()[0].confs()[0].col(31),
+                       mols()[2].confs()[0].col(31));
+
+  NURI_EXPECT_EIGEN_NE(mols()[0].confs()[0].col(72),
+                       mols()[1].confs()[0].col(72));
+  NURI_EXPECT_EIGEN_NE(mols()[1].confs()[0].col(72),
+                       mols()[2].confs()[0].col(72));
 }
 
 TEST_F(PDB1alxTest, HandleInconsistentResidues) {
-  const Substructure &res = mol().get_substructure(3);
-  EXPECT_EQ(res.name(), "TYR");
-  EXPECT_EQ(res.id(), 11);
-  EXPECT_EQ(res.num_atoms(), 21);
-  EXPECT_EQ(res.count_heavy_atoms(), 12);
+  {
+    const Substructure &res = mol().get_substructure(3);
+    EXPECT_EQ(res.name(), "TYR");
+    EXPECT_EQ(res.id(), 11);
+    EXPECT_EQ(res.num_atoms(), 21);
+    EXPECT_EQ(res.count_heavy_atoms(), 12);
 
-  auto subs = mol().find_substructures(11);
-  std::vector<Substructure> res_11(subs.begin(), subs.end());
-  EXPECT_EQ(res_11.size(), 1);
-  EXPECT_EQ(res_11[0].name(), "TYR");
+    auto subs = mol().find_substructures(11);
+    std::vector<Substructure> res_11(subs.begin(), subs.end());
+    EXPECT_EQ(res_11.size(), 1);
+    EXPECT_EQ(res_11[0].name(), "TYR");
+  }
+  {
+    for (const Molecule &mol: mols()) {
+      const Substructure &res = mol.get_substructure(3);
+      EXPECT_EQ(res.name(), "TYR");
+      EXPECT_EQ(res.id(), 11);
+      EXPECT_EQ(res.num_atoms(), 21);
+      EXPECT_EQ(res.count_heavy_atoms(), 12);
+
+      auto subs = mol.find_substructures(11);
+      std::vector<Substructure> res_11(subs.begin(), subs.end());
+      EXPECT_EQ(res_11.size(), 1);
+      EXPECT_EQ(res_11[0].name(), "TYR");
+    }
+  }
 }
 }  // namespace
 }  // namespace nuri
