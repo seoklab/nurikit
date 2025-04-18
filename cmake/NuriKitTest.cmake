50f73cd4f8ae11045c7bb6d9d3d646cdae1dd81b
Nuri Jung <jnooree@snu.ac.kr>
2025-04-18 11:24:43 +0900

    build: overhaul build system

--- cmake/NuriKitTest.cmake
+++ cmake/NuriKitTest.cmake
@@ -3,15 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if(NOT TARGET nuri_all_test)
-  add_custom_target(nuri_all_test)
-  clear_coverage_data(nuri_all_test)
-
-  if(BUILD_TESTING)
-    set_target_properties(nuri_all_test PROPERTIES EXCLUDE_FROM_ALL OFF)
-  endif()
-endif()
-
 include(GoogleTest)
 
 if(NURI_ENABLE_SANITIZERS AND CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
@@ -25,47 +16,50 @@ function(_nuri_generate_test_name root file)
 
   get_filename_component(test_name ${file} NAME_WE)
 
-  set(NURI_TEST_TARGET "nuri_${test_prefix}_${test_name}" PARENT_SCOPE)
+  set(NURI_TEST_TARGET "NuriTest-${test_prefix}-${test_name}" PARENT_SCOPE)
+  set(NURI_TEST_OUTPUT "nuri-${test_prefix}-${test_name}" PARENT_SCOPE)
 endfunction()
 
 function(nuri_add_test file)
   _nuri_generate_test_name("${PROJECT_SOURCE_DIR}/test" "${file}")
 
   add_executable("${NURI_TEST_TARGET}" "${file}")
-  target_link_libraries("${NURI_TEST_TARGET}" PRIVATE
+  target_link_libraries(
+    "${NURI_TEST_TARGET}"
+    PRIVATE
+    NuriLib
     GTest::gtest GTest::gmock GTest::gtest_main
-    absl::absl_log absl::absl_check)
+    absl::absl_log absl::absl_check
+  )
+  set_target_properties(
+    "${NURI_TEST_TARGET}"
+    PROPERTIES
+    OUTPUT_NAME "${NURI_TEST_OUTPUT}"
+  )
 
-  if(TARGET nuri_lib)
-    target_link_libraries("${NURI_TEST_TARGET}" PRIVATE nuri_lib)
-  endif()
-
-  gtest_discover_tests("${NURI_TEST_TARGET}"
+  gtest_discover_tests(
+    "${NURI_TEST_TARGET}"
     WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-    ${NURI_GTEST_EXTRA_ARGS})
+    ${NURI_GTEST_EXTRA_ARGS}
+  )
 
-  add_dependencies(nuri_all_test "${NURI_TEST_TARGET}")
+  add_dependencies(NuriAllTest "${NURI_TEST_TARGET}")
 endfunction()
-
-if(NOT TARGET nuri_all_fuzz)
-  add_custom_target(nuri_all_fuzz)
-  clear_coverage_data(nuri_all_fuzz)
-
-  if(NURI_BUILD_FUZZING AND BUILD_TESTING)
-    set_target_properties(nuri_all_fuzz PROPERTIES EXCLUDE_FROM_ALL OFF)
-  endif()
-endif()
 
 function(nuri_add_fuzz file)
   _nuri_generate_test_name("${PROJECT_SOURCE_DIR}/fuzz" "${file}")
 
   add_executable("${NURI_TEST_TARGET}" "${file}")
   target_link_options("${NURI_TEST_TARGET}" PRIVATE -fsanitize=fuzzer)
-  target_link_libraries("${NURI_TEST_TARGET}" PRIVATE absl::log_initialize)
+  target_link_libraries(
+    "${NURI_TEST_TARGET}"
+    PRIVATE NuriLib absl::log_initialize
+  )
+  set_target_properties(
+    "${NURI_TEST_TARGET}"
+    PROPERTIES
+    OUTPUT_NAME "${NURI_TEST_OUTPUT}"
+  )
 
-  if(TARGET nuri_lib)
-    target_link_libraries("${NURI_TEST_TARGET}" PRIVATE nuri_lib)
-  endif()
-
-  add_dependencies(nuri_all_fuzz "${NURI_TEST_TARGET}")
+  add_dependencies(NuriAllFuzz "${NURI_TEST_TARGET}")
 endfunction()
--- cmake/NuriKitUtils.cmake
+++ cmake/NuriKitUtils.cmake
@@ -49,9 +49,16 @@ function(nuri_get_version)
     set(NURI_FULL_VERSION "${SKBUILD_PROJECT_VERSION_FULL}")
     string(REGEX REPLACE "\\+.+$" "" NURI_VERSION "${NURI_FULL_VERSION}")
     message(
-      STATUS "NuriKit version from scikit-build-core: ${NURI_FULL_VERSION}")
+      STATUS "NuriKit version from scikit-build-core: ${NURI_FULL_VERSION}"
+    )
   else()
     _nuri_get_git_version_impl()
+  endif()
+
+  if(NURI_FORCE_VERSION)
+    message(STATUS "Using explicit NuriKit version: ${NURI_FORCE_VERSION}")
+    set(NURI_VERSION "${NURI_FORCE_VERSION}")
+    set(NURI_FULL_VERSION "${NURI_VERSION}")
   endif()
 
   if(NURI_REF)
@@ -85,195 +92,74 @@ function(nuri_get_version)
   set(NURI_REF "${NURI_REF}" PARENT_SCOPE)
 endfunction()
 
-function(nuri_make_available_deponly target)
-  include(FetchContent)
-
-  FetchContent_GetProperties(${target})
-
-  if(NOT ${target}_POPULATED)
-    FetchContent_Populate(${target})
-    add_subdirectory(
-      ${${target}_SOURCE_DIR} ${${target}_BINARY_DIR} EXCLUDE_FROM_ALL)
-  endif()
-endfunction()
-
-function(_target_system_include target dep)
-  get_target_property(
-    interface_include_dirs "${dep}" INTERFACE_INCLUDE_DIRECTORIES)
-  target_include_directories("${target}" SYSTEM PUBLIC ${interface_include_dirs})
-endfunction()
-
-function(target_system_include_directories target)
-  foreach(dep IN LISTS ARGN)
-    _target_system_include("${target}" "${dep}")
-  endforeach()
-endfunction()
-
-function(find_or_fetch_eigen)
-  set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
-  set(BUILD_TESTING OFF)
-  set(EIGEN_BUILD_DOC OFF)
-  set(EIGEN_BUILD_PKGCONFIG OFF)
-
-  find_package(Eigen3 3.4 QUIET)
-
-  if(Eigen3_FOUND)
-    message(STATUS "Found Eigen3 ${Eigen3_VERSION}")
-  else()
-    include(FetchContent)
-    message(NOTICE "Could not find compatible Eigen3. Fetching from gitlab.")
-
-    FetchContent_Declare(
-      eigen
-      GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
-      GIT_TAG 3.4.0)
-    nuri_make_available_deponly(eigen)
-  endif()
-endfunction()
-
-function(find_or_fetch_spectra)
-  set(BUILD_TESTING OFF)
-
-  find_package(Spectra 1.0 QUIET)
-
-  if(Spectra_FOUND)
-    message(STATUS "Found Spectra ${Spectra_VERSION}")
-  else()
-    include(FetchContent)
-    message(NOTICE "Could not find compatible Spectra. Fetching from github.")
-
-    FetchContent_Declare(
-      spectra
-      GIT_REPOSITORY https://github.com/yixuan/spectra.git
-      GIT_TAG v1.0.1)
-    nuri_make_available_deponly(spectra)
-    add_library(Spectra::Spectra ALIAS Spectra)
-  endif()
-endfunction()
-
-function(find_or_fetch_pybind11)
-  set(BUILD_TESTING OFF)
-  set(PYBIND11_FINDPYTHON ON)
-
-  # FindPythonInterp/FindPythonLibs deprecated since cmake 3.12
-  if(POLICY CMP0148)
-    cmake_policy(SET CMP0148 NEW)
-  endif()
-
-  find_package(pybind11 2.13 QUIET)
-
-  if(pybind11_FOUND)
-    message(STATUS "Found pybind11 ${pybind11_VERSION}")
-  else()
-    include(FetchContent)
-    message(NOTICE "Could not find compatible pybind11. Fetching from github.")
-
-    Fetchcontent_Declare(
-      pybind11
-      GIT_REPOSITORY https://github.com/pybind/pybind11.git
-      GIT_TAG v2.13.6
-    )
-    nuri_make_available_deponly(pybind11)
-  endif()
-endfunction()
-
 function(find_or_fetch_abseil)
-  set(BUILD_TESTING OFF)
-  set(BUILD_SHARED_LIBS OFF)
-  set(ABSL_BUILD_TESTING OFF)
-  set(ABSL_PROPAGATE_CXX_STD ON)
-  set(ABSL_USE_SYSTEM_INCLUDES ON)
-
   if(NURI_ENABLE_SANITIZERS)
     message(
       NOTICE
       "abseil must be built with sanitizers enabled; ignoring system abseil"
     )
-  elseif(NURI_PREBUILT_ABSL AND NOT absl_ROOT)
-    include(FetchContent)
-    message(NOTICE "Fetching prebuilt abseil binary.")
 
-    if(CMAKE_SYSTEM_NAME MATCHES Linux)
-      set(os_arch "manylinux2014_x86_64")
-    elseif(CMAKE_SYSTEM_NAME MATCHES Darwin)
-      set(os_arch "macosx_universal2")
-    endif()
-
-    Fetchcontent_Declare(
-      absl
-      URL "https://github.com/jnooree/abseil-cpp/releases/latest/download/libabsl-static-${os_arch}.tar.gz"
-    )
-    Fetchcontent_MakeAvailable(absl)
-
-    set(absl_ROOT "${absl_SOURCE_DIR}")
-    find_package(absl)
-
-    if(absl_FOUND AND absl_VERSION VERSION_GREATER_EQUAL 20240116)
-      message(STATUS "Found abseil ${absl_VERSION}")
-
-      if(CMAKE_SYSTEM_NAME MATCHES Linux)
-        string(APPEND CMAKE_CXX_FLAGS " -D_GLIBCXX_USE_CXX11_ABI=0")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" PARENT_SCOPE)
-      endif()
-
-      return()
+    if(CPM_USE_LOCAL_PACKAGES)
+      message(
+        WARNING
+        "CPM_USE_LOCAL_PACKAGES is not supported with sanitizers, ignoring"
+      )
     endif()
   else()
-    find_package(absl QUIET)
+    if(NURI_PREBUILT_ABSL)
+      include(FetchContent)
+      message(NOTICE "Fetching prebuilt abseil binary.")
 
-    # 20240116 required for VLOG()
+      if(CMAKE_SYSTEM_NAME MATCHES Linux)
+        set(os_arch "manylinux2014_x86_64")
+      elseif(CMAKE_SYSTEM_NAME MATCHES Darwin)
+        set(os_arch "macosx_universal2")
+      endif()
+
+      Fetchcontent_Declare(
+        absl
+        URL "https://github.com/jnooree/abseil-cpp/releases/latest/download/libabsl-static-${os_arch}.tar.gz"
+      )
+      Fetchcontent_MakeAvailable(absl)
+
+      set(absl_findpackage_args PATHS "${absl_SOURCE_DIR}" NO_DEFAULT_PATH)
+    else()
+      set(absl_findpackage_args QUIET)
+    endif()
+
+    find_package(absl ${absl_findpackage_args})
+
     if(absl_FOUND AND absl_VERSION VERSION_GREATER_EQUAL 20240116)
       message(STATUS "Found abseil ${absl_VERSION}")
+
+      if(NURI_PREBUILT_ABSL AND CMAKE_SYSTEM_NAME MATCHES Linux)
+        set(ABSL_USES_OLD_ABI ON PARENT_SCOPE)
+        set(
+          CMAKE_CXX_FLAGS
+          "${CMAKE_CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
+          PARENT_SCOPE
+        )
+      endif()
       return()
     endif()
   endif()
 
-  include(FetchContent)
   message(NOTICE "Could not find compatible abseil. Fetching from github.")
 
-  Fetchcontent_Declare(
-    absl
+  include(CPM)
+  set(CPM_USE_LOCAL_PACKAGES OFF)
+  CPMAddPackage(
+    NAME absl
+    OPTIONS
+    "BUILD_TESTING OFF"
+    "BUILD_SHARED_LIBS OFF"
+    "ABSL_ENABLE_INSTALL ON"
+    "ABSL_BUILD_TESTING OFF"
+    "ABSL_PROPAGATE_CXX_STD ON"
+    "ABSL_USE_SYSTEM_INCLUDES ON"
     URL https://github.com/jnooree/abseil-cpp/releases/latest/download/abseil-cpp-latest.tar.gz
-  )
-  nuri_make_available_deponly(absl)
-endfunction()
-
-function(handle_boost_dependency target)
-  set(BUILD_TESTING OFF)
-
-  # FindBoost deprecated since cmake 3.30
-  if(POLICY CMP0167)
-    cmake_policy(SET CMP0167 NEW)
-  endif()
-
-  find_package(Boost 1.82 QUIET)
-
-  if(Boost_FOUND)
-    message(STATUS "Found Boost ${Boost_VERSION}")
-    target_include_directories("${target}" SYSTEM PUBLIC ${Boost_INCLUDE_DIRS})
-    return()
-  endif()
-
-  message(NOTICE "Could not find compatible Boost. Fetching from boostorg.")
-  include(FetchContent)
-  FetchContent_Declare(
-    boost
-    URL https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.xz
-  )
-
-  set(Boost_ENABLE_CMAKE ON)
-  set(Boost_USE_STATIC_LIBS ON)
-  nuri_make_available_deponly(boost)
-
-  target_system_include_directories(
-    "${target}"
-    Boost::spirit Boost::fusion Boost::mpl Boost::optional Boost::container
-    Boost::iterator Boost::config Boost::range
-  )
-  target_link_libraries(
-    "${target}"
-    PUBLIC Boost::container Boost::iterator Boost::config Boost::range
-    PRIVATE Boost::spirit Boost::fusion Boost::mpl Boost::optional
+    EXCLUDE_FROM_ALL ON
+    SYSTEM ON
   )
 endfunction()
 
@@ -292,16 +178,19 @@ function(set_sanitizer_envs)
   execute_process(
     COMMAND "${CMAKE_CXX_COMPILER}" "-print-file-name=${filename}"
     OUTPUT_VARIABLE asan_lib_path
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 
   execute_process(
     COMMAND "${CMAKE_CXX_COMPILER}" "-print-file-name=libubsan.so"
     OUTPUT_VARIABLE ubsan_lib_path
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 
   set(SANITIZER_ENVS
     "LD_PRELOAD=${asan_lib_path} ${ubsan_lib_path} $ENV{LD_PRELOAD}"
-    PARENT_SCOPE)
+    PARENT_SCOPE
+  )
 endfunction()
 
 function(clear_coverage_data target)
@@ -318,3 +207,8 @@ function(clear_coverage_data target)
     VERBATIM
   )
 endfunction()
+
+macro(add_both_options)
+  add_compile_options(${ARGV})
+  add_link_options(${ARGV})
+endmacro()
