da25385f756c4844b0dbef22e27161c9f3e0d848
Nuri Jung <jnooree@snu.ac.kr>
2025-05-14 17:29:59 +0900

    test(algo/pcharge/gasteiger): test for conjugated system

--- test/algo/pcharge_test.cpp
+++ test/algo/pcharge_test.cpp
@@ -22,5 +22,28 @@ TEST(ChargeGasteiger, Ethane) {
   EXPECT_NEAR(mol[0].data().partial_charge(), -0.068, 5e-3);
   EXPECT_NEAR(mol[1].data().partial_charge(), -0.068, 5e-3);
 }
+
+TEST(ChargeGasteiger, AcetateIon) {
+  Molecule mol = read_smiles({ "CC(=O)[O-]" });
+  ASSERT_TRUE(MoleculeSanitizer(mol).sanitize_all());
+
+  ASSERT_TRUE(assign_charges_gasteiger(mol));
+
+  EXPECT_NEAR(mol[2].data().partial_charge(), mol[3].data().partial_charge(),
+              1e-6);
+}
+
+TEST(ChargeGasteiger, GuanidiniumChloride) {
+  Molecule mol = read_smiles({ "NC(=[NH2+])N.[Cl-]" });
+  ASSERT_TRUE(MoleculeSanitizer(mol).sanitize_all());
+
+  ASSERT_TRUE(assign_charges_gasteiger(mol));
+
+  EXPECT_NEAR(mol[0].data().partial_charge(), mol[2].data().partial_charge(),
+              1e-6);
+  EXPECT_NEAR(mol[0].data().partial_charge(), mol[3].data().partial_charge(),
+              1e-6);
+  EXPECT_NEAR(mol[4].data().partial_charge(), -1, 1e-6);
+}
 }  // namespace
 }  // namespace nuri
