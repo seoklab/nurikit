092cc0ed203df4306af0779d41dbd2f97d2fd287
Nuri Jung <jnooree@snu.ac.kr>
2025-05-19 18:12:54 +0900

    build: include Boost::graph in find_package call

--- cmake/NuriKitUtils.cmake
+++ cmake/NuriKitUtils.cmake
@@ -96,13 +96,23 @@ function(nuri_get_version)
 endfunction()
 
 function(find_or_add_package)
-  cmake_parse_arguments(_pkg "" "NAME;MIN_VERSION;CPM_VERSION" "" "${ARGN}")
+  cmake_parse_arguments(
+    _pkg ""
+    "NAME;MIN_VERSION;CPM_VERSION;COMPONENTS"
+    ""
+    "${ARGN}"
+  )
 
   if(NOT _pkg_MIN_VERSION)
     set(_pkg_MIN_VERSION "${_pkg_CPM_VERSION}")
   endif()
 
-  find_package("${_pkg_NAME}" "${_pkg_MIN_VERSION}" QUIET)
+  find_package(
+    "${_pkg_NAME}"
+    "${_pkg_MIN_VERSION}"
+    QUIET
+    COMPONENTS "${_pkg_COMPONENTS}"
+  )
 
   if(${_pkg_NAME}_FOUND)
     message(STATUS "Found ${_pkg_NAME} ${${_pkg_NAME}_VERSION}")
