b70a86cd9925850a7c0c5a8f89cb266ee7d39bae
Nuri Jung <jnooree@snu.ac.kr>
2025-09-18 15:15:24 +0900

    refactor(tools/galign): refine and simplify interface

--- src/tools/galign/galign.cpp
+++ src/tools/galign/galign.cpp
@@ -6,20 +6,19 @@
 #include "nuri/tools/galign.h"
 
 namespace nuri {
-std::vector<AlignResult>
-galign(const Molecule &mol, const Matrix3Xd &seed, const GARigidMolInfo &templ,
-       const bool flexible, const int max_conf, const int rigid_max_conf,
-       const double rigid_min_msd, const GASamplingArgs &genetic,
-       const GAMinimizeArgs &minimize) {
+std::vector<AlignResult> galign(const Molecule &mol, const Matrix3Xd &seed,
+                                const GARigidMolInfo &templ,
+                                const bool flexible, const int max_conf,
+                                const GASamplingArgs &sampling,
+                                const GAMinimizeArgs &minimize) {
   GARigidMolInfo query(mol, seed, templ.vdw_scale(), templ.hetero_scale(),
                        templ.dcut());
 
   if (!flexible)
-    return internal::rigid_galign_impl(query, templ, max_conf,
-                                       templ.hetero_scale(), rigid_min_msd);
+    return internal::rigid_galign_impl(
+        query, templ, max_conf, templ.hetero_scale(), sampling.rigid_min_msd);
 
-  return internal::flexible_galign_impl(query, templ, max_conf,
-                                        templ.hetero_scale(), genetic, minimize,
-                                        rigid_max_conf, rigid_min_msd);
+  return internal::flexible_galign_impl(
+      query, templ, max_conf, templ.hetero_scale(), sampling, minimize);
 }
 }  // namespace nuri
