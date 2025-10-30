18ea0b060a6fd912f1fddf3bff569e8f94c707a6
Nuri Jung <jnooree@snu.ac.kr>
2025-10-30 15:05:52 +0900

    tmp

---
+++ include/nuri/algo/substr/match.h
@@ -0,0 +1,13 @@
+//
+// Project NuriKit - Copyright 2025 SNU Compbio Lab.
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#ifndef NURI_ALGO_SUBSTR_MATCH_H_
+#define NURI_ALGO_SUBSTR_MATCH_H_
+
+#include "nuri/core/graph/vf2pp.h"
+
+namespace nuri { }
+
+#endif /* NURI_ALGO_SUBSTR_MATCH_H_ */
