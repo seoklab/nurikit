dcc4127cdf8d5e52219f76d675a8b9a2744d4a56
Nuri Jung <jnooree@snu.ac.kr>
2025-11-25 09:44:16 +0900

    build: refactor coverage data clearing script

--- cmake/NuriKitClearCoverage.cmake
+++ cmake/NuriKitClearCoverage.cmake
@@ -8,19 +8,15 @@ if(NOT CMAKE_SCRIPT_MODE_FILE)
   return()
 endif()
 
-if(NOT NURI_COVERAGE_DATA_DIR)
-  message(FATAL_ERROR "No parent directory provided.")
-endif()
-
-file(GLOB_RECURSE nuri_gcda LIST_DIRECTORIES OFF "${NURI_COVERAGE_DATA_DIR}/*.gcda")
+file(GLOB_RECURSE nuri_gcda LIST_DIRECTORIES OFF "*.gcda")
 
 if(nuri_gcda)
   list(LENGTH nuri_gcda nuri_gcda_count)
-  message(NOTICE "Removing ${nuri_gcda_count} gcda files in ${NURI_COVERAGE_DATA_DIR}")
+  message(NOTICE "Removing ${nuri_gcda_count} gcda files in ${CMAKE_SOURCE_DIR}")
   file(REMOVE ${nuri_gcda})
 endif()
 
-file(GLOB_RECURSE nuri_gcno LIST_DIRECTORIES OFF "${NURI_COVERAGE_DATA_DIR}/*.gcno")
+file(GLOB_RECURSE nuri_gcno LIST_DIRECTORIES OFF "*.gcno")
 
 foreach(gcno IN LISTS nuri_gcno)
   string(REGEX REPLACE "\\.gcno$" ".o" object "${gcno}")
