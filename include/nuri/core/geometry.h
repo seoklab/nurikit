bcaa453f88e9b3e7a316f22b8d46ff8a90fce5e5
Nuri Jung <jnooree@snu.ac.kr>
2025-11-26 14:27:56 +0900

    docs(core/geometry): update fibonacci lattice docs and add references

--- include/nuri/core/geometry.h
+++ include/nuri/core/geometry.h
@@ -794,17 +794,20 @@ extern bool embed_distances_4d(Eigen::Ref<Matrix4Xd> pts, MatrixXd &dsqs);
  * The points are placed on the sphere as the following equation:
  *
  * \f[
- * t_i (\theta, \phi) = \left(\frac{i}{\varphi}, \frac{i + 0.5}{N} \right)
+ * t_i (\theta, \phi) = \left(\frac{i}{\varphi^2}, \frac{i + 0.5}{N} \right)
  * \f]
  *
  * Where \f$\varphi\f$ is the golden ratio, \f$N\f$ is the number of points, and
- * \f$0 \leq i < N \f$.
+ * \f$0 \leq i < N \f$. Note, for compatibility with other implementations, step
+ * size along \f$\theta\f$ is defined as \f$1 / \varphi^2\f$ instead of the
+ * complementary \f$1 / \varphi\f$ used in some literature.
  *
- * This implementation is based on the following reference:
- * M Roberts. *How to evenly distribute points on a sphere more effectively than
- * the canonical Fibonacci Lattice* (2020-06-07).
- * https://extremelearning.com.au/how-to-evenly-distribute-points-on-a-sphere-more-effectively-than-the-canonical-fibonacci-lattice/
- * (Accessed 2025-11-25)
+ * This implementation is based on the following references:
+ * - M Roberts. *How to evenly distribute points on a sphere more effectively
+ *   than the canonical Fibonacci Lattice* (2020-06-07). (Accessed 2025-11-25).
+ *   \cite core:geom:fiblat-2020
+ * - G Alvaro. *Math. Geosci.* **2010**, *42*, 49-64.
+ *   \cite core:geom:fiblat-2010
  */
 extern Matrix3Xd canonical_fibonacci_lattice(int npts);
 }  // namespace nuri
